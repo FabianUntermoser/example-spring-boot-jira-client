cc1546a1f64d16124ec87b550b24b8c8b42a6262
Fabian Untermoser <fabian.untermoser1@porscheinformatik.com>
2022-04-12 10:06:27 +0200

    init from start.spring.io

---
+++ src/main/java/com/example/examplespringbootjiraclient/ExampleSpringBootJiraClientApplication.java
@@ -0,0 +1,13 @@
+package com.example.examplespringbootjiraclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExampleSpringBootJiraClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExampleSpringBootJiraClientApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/examplespringbootjiraclient/ExampleSpringBootJiraClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.examplespringbootjiraclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExampleSpringBootJiraClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
